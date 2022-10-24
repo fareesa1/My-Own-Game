80c111f174be4d7156660161b8cf7910c6b1b80e
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2022-10-24 22:12:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var obstaclesGroup, obstacle1, obstacle2
 var coin, coinGroup
+var score = 0
+var lives = 4
+var handleControl = true
 
 function setup() {
   createCanvas(800,400);
@@ -10,7 +13,9 @@ function setup() {
   plr = createSprite(400, 200, 50, 50);
   plr.addAnimation("run", plr_run)
   plr.addAnimation("idle", plr_idle)
+  plr.addAnimation("dead", plr_dead)
   plr.scale = 0.3
+  plr.debug=true
   plr.setCollider("rectangle", 50,0, 200,430)
   ground = createSprite(2500,plr.y+100,5000,10)
   ground.visible = false
@@ -19,6 +24,7 @@ function setup() {
   coinGroup = new Group();
 
   wall = createSprite(350,200,20,500)
+  wall.visible = false
 
   spawnObstacles();
   spawnCoins();
@@ -28,7 +34,7 @@ function preload(){
   plr_run = loadAnimation("png/run1.png", "png/run2.png", "png/run3.png", "png/run4.png", "png/run5.png", "png/run6.png", "png/run7.png", "png/run8.png",)
   bg_image = loadImage("bg.jpg")
   plr_idle = loadAnimation("png/idle2.png")
-
+  plr_dead = loadAnimation("png/dead4.png")
   obstacle1 = loadImage("png/obstacle1.png")
   obstacle2 = loadImage("png/obstacle2.png")  
 
@@ -49,7 +55,13 @@ function draw() {
     
   }
 
-    if(keyDown("space")&& plr.y>190){
+  if(handleControl == true){
+
+  if(keyDown("down")&& plr.y>190){
+    plr.velocityY = -18
+    }
+
+    if(keyDown("up")&& plr.y>190){
       plr.velocityY = -10
       }
       plr.velocityY+= 1
@@ -73,9 +85,12 @@ function draw() {
       plr.changeAnimation("idle")
       plr.velocityX=0
     }
-
-    
-    text('Score: ', plr.x,plr.y)
+  }
+    stroke(3)
+    textSize(32)
+    fill("black")
+    text('Score: '+score, plr.x+200,plr.y-250)
+    text('Lives: '+lives, plr.x+50,plr.y-250)
 
   if(plr.y>100){
     camera.y=plr.y-100;
@@ -87,6 +102,27 @@ function draw() {
 
 //if(plr.x-cactus.x>200) cactus.x=plr.x+200;
 //if(cactus.x-plr.x>200) cactus.x=plr.x-200;
+
+if(coinGroup.isTouching(plr)){
+  score+=1
+coinGroup[0].destroy()
+
+}
+
+if(plr.isTouching(obstaclesGroup)){
+  obstaclesGroup[1].destroy()
+  lives-=1
+  }
+
+  if(lives <=0){
+    textSize(60)
+    fill("white")
+    text('Game Over', plr.x-100,plr.y-50)
+    plr.velocityX = 0
+handleControl = false
+plr.changeAnimation("dead")
+  }
+
 }
 
 function spawnObstacles(){
@@ -108,7 +144,7 @@ function spawnObstacles(){
 }
 
 function spawnCoins(){
-  for(var i=0; i<4000; i+=625)  {
+  for(var i=0; i<4000; i+=325)  {
     var coin = createSprite(i, 110, 10, 40);
     //var rand = Math.round(random(1,1));
     //switch(rand){
@@ -118,6 +154,7 @@ function spawnCoins(){
     //}
     coin.scale = 0.1;
     coin.addImage(coinImg)
+    coinGroup.add(coin)
     coin.depth=plr.depth+1
   }
 }
